e1e34dd34b0b4e7a13e9b11a39d6b2f930a2217b
Aman Deep Bhagat <bhagatamandeep50@gmail.com>
2026-09-17 14:28:04 +0530

    Ciel in a bst code

---
+++ CielinABST.cpp
@@ -0,0 +1,18 @@
+
+TreeNode* ceil(TreeNode* root){
+     TreeNode* ceil = nullptr;
+     while(root){
+     if(root->data == val){
+         ceil = root;
+         return ciel;
+      }
+     if(key > root->data){
+     root = root->right;
+     }
+    else {
+     ceil = root->data;
+     root = root-left;
+   }
+ }
+ return ceil;
+}
