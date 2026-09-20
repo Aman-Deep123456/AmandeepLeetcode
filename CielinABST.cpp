efa7b571cb15026e1e1fbda84502d96329ec399b
Aman Deep Bhagat <bhagatamandeep50@gmail.com>
2026-09-20 23:03:54 +0530

    added the optimal

--- CielinABST.cpp
+++ CielinABST.cpp
@@ -1,18 +1,20 @@
-
-TreeNode* ceil(TreeNode* root){
-     TreeNode* ceil = nullptr;
-     while(root){
-     if(root->data == val){
-         ceil = root;
-         return ciel;
-      }
-     if(key > root->data){
-     root = root->right;
-     }
-    else {
-     ceil = root->data;
-     root = root-left;
-   }
- }
- return ceil;
-}
+class solution {
+public:
+    TreeNode* ceil(TreeNode* root){
+        TreeNode* ceil = nullptr;
+        while(root){
+            if(root->data == val){
+                ceil = root;
+                return ciel;
+            }
+            if(key > root->data){
+                root = root->right;
+            }
+            else {
+                ceil = root->data;
+                root = root-left;
+            }
+        }
+        return ceil;
+    }
+};
