015e2ed639fefe56a0f422331d5308a48b583633
Aman Deep Bhagat <bhagatamandeep50@gmail.com>
2026-09-22 22:04:27 +0530

    visulaized the approach

--- Moorse.cpp
+++ Moorse.cpp
@@ -22,7 +22,7 @@ public:
     if(!root) return {};
     vector<int> inorder;
     while(curr){
-        
+        //if left is not there push the values
         if(curr->left == nullptr){
          inorder.push_back(curr->data);
          curr = curr->right;
@@ -32,6 +32,7 @@ public:
             while(prev->right && prev->right!=curr){
                 prev = prev->right;
             }
+            // if no right connect the parent 
             if(prev->right == nullptr){
                 prev->right = curr;
                 curr=curr->left;
